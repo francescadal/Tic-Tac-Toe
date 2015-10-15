976fc6e9f1c3f9c0600e57a90c0daf38e6cb52cf
francescadal <francescadalembert@icloud.com>
2015-10-15 09:52:06 -0400

    Added some more javascript

--- NaughtsCrosses.js
+++ NaughtsCrosses.js
@@ -1,19 +1,37 @@
+
+var symbol = "X";
+
 $(document).ready(function() {
   $("#board").on('click', function(event) {
     var $target = $(event.target);
     var id = $target.data("id");
-    if (id !== undefined){
-     console.log("clicked " + id);
-     if($target.text() === ""){
-      $target.text(id);
-     }
-   }
+
+    if (id !== undefined) {
+      console.log("clicked " + id);
+      if($target.text() === "") {
+        $target.text(symbol);
+         if (symbol === "X"){
+          symbol = "O";
+        } else{
+          symbol = "X";
+        }
+
+
+
+        // if (isOdd === true){
+        //   $target.text('x');
+        //   isOdd = false;
+        //   board[$target.index()] = 'x';
+        //   checkForWin('x')
+        // } else {
+        //   $target.html('o');
+        //   isOdd = true;
+        //   board[$target.index()] = 'o';
+        //   checkForWin('o')
+        // }
+      }
+    }
   });
 });
 
-var player = function(event) {
-  if ( ){
-    $target === id;
-    return x
-  };
-}
+
